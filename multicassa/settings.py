784e468e4eb11d284ab864c7e5e90da4abc79b59
Igor Lagutin <7984521@gmail.com>
2019-07-16 21:56:25 +0300

    added ststic dir

--- multicassa/settings.py
+++ multicassa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5smzb5unw(a4)75nejafo9vcnfwh#2cx220zm^+saoj3sh3_%9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['test.ttn.od.ua']
+
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 try:
