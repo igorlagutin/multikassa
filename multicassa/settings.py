7c4752955e15d9572738804bf89928048d77c07d
Igor Lagutin <7984521@gmail.com>
2019-07-17 20:03:25 +0300

    whitenoise settings

--- multicassa/settings.py
+++ multicassa/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,7 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-
+STATIC_ROOT = os.path.join('/home/atest361/public_html/multicassa/public/', 'public')
