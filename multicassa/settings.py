bbf829a0615c8bfd05dddfc66e4ceb8d955824e9
Igor Lagutin <7984521@gmail.com>
2019-07-20 21:28:44 +0300

    added list display in admin, moved secet key to local_settings

--- multicassa/settings.py
+++ multicassa/settings.py
@@ -8,8 +8,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5smzb5unw(a4)75nejafo9vcnfwh#2cx220zm^+saoj3sh3_%9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -94,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
--- rate_page/admin.py
+++ rate_page/admin.py
@@ -3,10 +3,21 @@ from django.contrib import admin
 from .models import *
 
 
+class RateAdmin(admin.ModelAdmin):
+	# fields = ('name', 'filters', 'category')
+
+    list_display = ('name','pursage','sell',)
+    list_editable = ('pursage','sell',)
+    ordering = ['id']
+
+class CryptoRateAdmin(admin.ModelAdmin):
+    list_display = ('name','sell', 'code')
 
 
-admin.site.register(Rate)
-admin.site.register(CryptoRate)
+
+
+admin.site.register(Rate, RateAdmin)
+admin.site.register(CryptoRate, CryptoRateAdmin)
 
 AdminSite.site_header = "MULTIKASSA"
 
