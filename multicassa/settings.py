69f912a9b51f2d6e0f44b4d49fab30224466a44f
Igor Lagutin <7984521@gmail.com>
2019-08-03 15:32:05 +0300

    move DEBUG option to local settings

--- multicassa/settings.py
+++ multicassa/settings.py
@@ -9,8 +9,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 
 
